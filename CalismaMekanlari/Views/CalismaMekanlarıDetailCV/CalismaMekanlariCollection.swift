78954948d765eb90192fa6b791538654186201b5
ozancck <ozancck@icloud.com>
2025-01-30 19:40:25 +0300

    detail header image

--- CalismaMekanlari/Views/CalismaMekanlarıDetailCV/CalismaMekanlariCollection.swift
+++ CalismaMekanlari/Views/CalismaMekanlarıDetailCV/CalismaMekanlariCollection.swift
@@ -14,19 +14,16 @@ class CalismaMekanlariCollection: UICollectionViewController {
     static let categoryHeaderId = "categoryHeaderId"
     let headerId = "headerId"
 
-    
-    
      init() {
          super.init(collectionViewLayout: CalismaMekanlariCollection.crateLayout())
      }
-     
-     
+
+
          required init?(coder: NSCoder) {
              fatalError("init(coder:) has not been implemented")
          }
-     
- 
-    
+
+
 //    required init?(coder: NSCoder) {
 //        super.init(collectionViewLayout: CalismaMekanlariCollection.crateLayout())
 //    }
@@ -37,9 +34,6 @@ class CalismaMekanlariCollection: UICollectionViewController {
             if sectionNumber == 0 {
                 let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(250)))
 
-             
-                item.contentInsets.bottom = 75
-
                 let group = NSCollectionLayoutGroup.vertical(
                     layoutSize: .init(
                         widthDimension: .fractionalWidth(1),
@@ -48,29 +42,23 @@ class CalismaMekanlariCollection: UICollectionViewController {
                 let section = NSCollectionLayoutSection(group: group)
 
                 section.orthogonalScrollingBehavior = .paging
-                
-               
 
                 return section
-            }else if sectionNumber == 1 {
-                
+            } else if sectionNumber == 1 {
                 let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
 
-                
-                
+                item.contentInsets.top = 15
 
-                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(80)), subitems: [item])
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(170)), subitems: [item])
 
                 let section = NSCollectionLayoutSection(group: group)
 
-                
-
+                section.contentInsets.bottom = 15
 
                 return section
-                
-            }else if sectionNumber == 2 {
+
+            } else if sectionNumber == 2 {
                 let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(0.33), heightDimension: .fractionalHeight(1)))
-                
 
                 item.contentInsets.trailing = 5
                 item.contentInsets.leading = 5
@@ -82,12 +70,9 @@ class CalismaMekanlariCollection: UICollectionViewController {
 
                 section.contentInsets.leading = 15
                 section.contentInsets.trailing = 15
-              
-
 
                 return section
-            }
-            else {
+            } else {
                 let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
 
                 item.contentInsets.trailing = 15
@@ -109,14 +94,11 @@ class CalismaMekanlariCollection: UICollectionViewController {
     }
 
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-      
-
         if indexPath.section == 0 {
             print("Selected item at  section 1\(indexPath)")
-         
+
         } else {
             print("Selected item at section 5\(indexPath)")
-          
         }
     }
 
@@ -124,29 +106,24 @@ class CalismaMekanlariCollection: UICollectionViewController {
         return 3
     }
 
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Nib ile oluşturulan cell'i register etme
         let nib = UINib(nibName: "CalismaMekanlariDetailHeaderCell", bundle: nil)
         collectionView.register(nib, forCellWithReuseIdentifier: "CalismaMekanlariDetailHeaderCell")
-        
+
         let nib2 = UINib(nibName: "CalismaMekanlariDetailBoxCell", bundle: nil)
         collectionView.register(nib2, forCellWithReuseIdentifier: "CalismaMekanlariDetailBoxCell")
-   
-        
-        
 
- 
-        //hide navigaton
-        self.navigationController?.isNavigationBarHidden = true
-        
-        //ignore safe are
+        let nib3 = UINib(nibName: "CalismaMekanlariDetailHeaderImageCell", bundle: nil)
+        collectionView.register(nib3, forCellWithReuseIdentifier: "CalismaMekanlariDetailHeaderImageCell")
+
+        // hide navigaton
+        navigationController?.isNavigationBarHidden = true
+
+        // ignore safe are
         collectionView.contentInsetAdjustmentBehavior = .never
-        
-        
 
         collectionView.register(Header.self, forSupplementaryViewOfKind: CalismaMekanlariCollection.categoryHeaderId, withReuseIdentifier: headerId)
 
@@ -159,7 +136,7 @@ class CalismaMekanlariCollection: UICollectionViewController {
         switch indexPath.section {
         case 1:
             header.label.text = "Yemekhane"
-      
+
         default:
             header.label.text = "Birinci Section Başlığı"
         }
@@ -168,12 +145,11 @@ class CalismaMekanlariCollection: UICollectionViewController {
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
         if section == 0 {
             return 1
-        }else if section == 1 {
+        } else if section == 1 {
             return 1
-        }else if section == 2 {
+        } else if section == 2 {
             return 5
         }
 
@@ -183,20 +159,15 @@ class CalismaMekanlariCollection: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if indexPath.section == 0 {
             return cellForSection1(collectionView: collectionView, indexPath: indexPath)
-        }
-        else if indexPath.section == 1 {
+        } else if indexPath.section == 1 {
             return cellForSection2(collectionView: collectionView, indexPath: indexPath)
-        }
-        else if indexPath.section == 2 {
+        } else if indexPath.section == 2 {
             return cellForSection3(collectionView: collectionView, indexPath: indexPath)
         }
-        
-        
 
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-        
-        //add image view like backgorund
-        
+
+        // add image view like backgorund
 
         cell.backgroundColor = .systemYellow
 
@@ -205,103 +176,91 @@ class CalismaMekanlariCollection: UICollectionViewController {
 
     func cellForSection1(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
         // ChangePasswordCell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
-        
-        
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "test")
-        imageView.contentMode = .scaleAspectFill
-        cell.backgroundView = imageView
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CalismaMekanlariDetailHeaderImageCell", for: indexPath) as! CalismaMekanlariDetailHeaderImageCell
 
+        cell.headerImage.image = UIImage(named: "test")
 
         return cell
     }
-    
+
     func cellForSection2(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
         // ChangePasswordCell
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CalismaMekanlariDetailHeaderCell", for: indexPath) as! CalismaMekanlariDetailHeaderCell
-     
 
         return cell
     }
-    
+
     func cellForSection3(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
         // ChangePasswordCell
-        
+
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CalismaMekanlariDetailBoxCell", for: indexPath) as! CalismaMekanlariDetailBoxCell
         cell.titleLbl.text = "Ortamdaki Gürültü Seviyesi"
 
         return cell
     }
-    
-    
+
     /*
-    
-    func cellForSection2(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
-        // ChangePasswordCell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FoodCellCollection", for: indexPath) as! FoodCellCollection
-        
-        cell.imageview.image = UIImage(systemName: "person")
-        cell.imageview.contentMode = .scaleAspectFill
-        cell.imageview.backgroundColor = .red
-        cell.imageview.addCornerRadius()
-    
-        cell.arrowImage.image = UIImage(named: "ArrowRight")
-        cell.backgroundCell.backgroundColor = .cellBackground
 
-        return cell
-    }
-    
-    func cellForSection3(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
-        // ChangePasswordCell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FavoriteScreensButtonCell", for: indexPath) as! FavoriteScreensButtonCell
-        cell.imageview.image = UIImage(systemName: "person")
-        
-        cell.imageview.contentMode = .scaleAspectFill
-        
-        cell.iconBackground.backgroundColor = .cellBackground
-        
-        cell.iconBackground.layer.cornerRadius =  cell.iconBackground.frame.size.width / 2
-        cell.iconBackground.clipsToBounds = true
-        
-        cell.screenTitle.text = "Bizden Haberler"
-        
-        
-        
-      
+     func cellForSection2(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
+         // ChangePasswordCell
+         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FoodCellCollection", for: indexPath) as! FoodCellCollection
 
-        return cell
-    }
-    
-    func cellForSection4(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NoticesFromUsCollectionCell", for: indexPath) as! NoticesFromUsCollectionCell
-        cell.imageview.image = UIImage(systemName: "person")
-        cell.imageview.backgroundColor = .red
-        cell.imageview.contentMode = .scaleAspectFill
-        cell.mainTitle.text = "Başlık"
-        cell.bodyTitle.text = "Alt Başlık altdsfdsfdsfdsfsdfsdfs"
-        
-        return cell
-    }
-    
-    func cellForSection5(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ShortCutCollectionCell", for: indexPath) as! ShortCutCollectionCell
-        cell.imageview.image = UIImage(systemName: "person")
-        cell.imageview.contentMode = .scaleAspectFill
-        cell.imageview.backgroundColor = .red
-        cell.mainTitle.text = "Başlık"
-        cell.bodyTitle.text = "Alt Başlık altdsfdsfdsfdsfsdfsdfs"
-        
-        return cell
-    }
-    
-     */
-    
+         cell.imageview.image = UIImage(systemName: "person")
+         cell.imageview.contentMode = .scaleAspectFill
+         cell.imageview.backgroundColor = .red
+         cell.imageview.addCornerRadius()
+
+         cell.arrowImage.image = UIImage(named: "ArrowRight")
+         cell.backgroundCell.backgroundColor = .cellBackground
+
+         return cell
+     }
+
+     func cellForSection3(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
+         // ChangePasswordCell
+         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FavoriteScreensButtonCell", for: indexPath) as! FavoriteScreensButtonCell
+         cell.imageview.image = UIImage(systemName: "person")
+
+         cell.imageview.contentMode = .scaleAspectFill
+
+         cell.iconBackground.backgroundColor = .cellBackground
+
+         cell.iconBackground.layer.cornerRadius =  cell.iconBackground.frame.size.width / 2
+         cell.iconBackground.clipsToBounds = true
+
+         cell.screenTitle.text = "Bizden Haberler"
+
+         return cell
+     }
+
+     func cellForSection4(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
+
+         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NoticesFromUsCollectionCell", for: indexPath) as! NoticesFromUsCollectionCell
+         cell.imageview.image = UIImage(systemName: "person")
+         cell.imageview.backgroundColor = .red
+         cell.imageview.contentMode = .scaleAspectFill
+         cell.mainTitle.text = "Başlık"
+         cell.bodyTitle.text = "Alt Başlık altdsfdsfdsfdsfsdfsdfs"
+
+         return cell
+     }
+
+     func cellForSection5(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
+
+         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ShortCutCollectionCell", for: indexPath) as! ShortCutCollectionCell
+         cell.imageview.image = UIImage(systemName: "person")
+         cell.imageview.contentMode = .scaleAspectFill
+         cell.imageview.backgroundColor = .red
+         cell.mainTitle.text = "Başlık"
+         cell.bodyTitle.text = "Alt Başlık altdsfdsfdsfdsfsdfsdfs"
+
+         return cell
+     }
+
+      */
 }
 
-struct ContentView_Previews: PreviewProvider {
+ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Container().edgesIgnoringSafeArea(.all)
     }
@@ -316,24 +275,22 @@ struct ContentView_Previews: PreviewProvider {
 
         typealias UIViewControllerType = UIViewController
     }
-}
+ }
 
-struct ContentView: View {
+ struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
     }
-}
+ }
 
 class Header: UICollectionReusableView {
     let label = UILabel()
-    
 
     override init(frame: CGRect) {
         super.init(frame: frame)
 
         label.font = UIFont.boldSystemFont(ofSize: 16)
         addSubview(label)
-
     }
 
     override func layoutSubviews() {
@@ -350,13 +307,9 @@ class Header: UICollectionReusableView {
             width: labelWidth - padding, // padding'i düşürüyoruz
             height: bounds.height
         )
-        
-
-       
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
-
---
+++ CalismaMekanlari/Views/CalismaMekanlarıDetailCV/Cell/CellForHeaderImage/CalismaMekanlariDetailHeaderImageCell.swift
@@ -0,0 +1,18 @@
+//
+//  CalismaMekanlariDetailHeaderImageCell.swift
+//  CalismaMekanlari
+//
+//  Created by Ozan Çiçek on 29.01.2025.
+//
+
+import UIKit
+
+class CalismaMekanlariDetailHeaderImageCell: UICollectionViewCell {
+
+    @IBOutlet weak var headerImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
